4feeceba3765703f10c3597ec7eecbd22d4ff5dc
Andrew <aja1945@gmail.com>
2023-10-25 19:30:41 -0400

    first commit

---
+++ src/components/TodoApp.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTodo = () => {
+    if (task.trim() !== '') {
+      setTodos([...todos, task]);
+      setTask('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="todo-app">
+      <h2>Todo List</h2>
+      <div className="add-todo">
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Add a new task"
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoApp;
+
+import TodoApp from './components/TodoApp';
+
+function App() {
+    return (
+      <div className="App">
+        <Navbar />
+        <LandingPage />
+        <TodoApp />
+      </div>
+    );
+  }
